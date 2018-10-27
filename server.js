175eef320077acf1d7f7dc1b0931cc67bb0596b0
wasim963 <wasimcasper17@gmail.com>
2018-10-27 17:39:55 +0530

    chnaged port
    Signed-off-by: wasim963 <wasimcasper17@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const srv = express();
+const SERVER_PORT = process.env.PORT || 1222
+
 
 srv.get('/',(req,res) =>{
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 
-srv.listen(1222, function () {
-    console.log("Running on 1222");
+srv.listen(SERVER_PORT, function () {
+    console.log("Server up and Running");
 });
